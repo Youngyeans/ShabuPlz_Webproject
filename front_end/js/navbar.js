d6a8bb9ae3a37eac671d1b766d6ace83a4aed370
KIT4ER0 <tae.tanakit47@gmail.com>
2024-03-10 21:49:50 +0700

    edit bug

--- front_end/js/navbar.js
+++ front_end/js/navbar.js
@@ -2,9 +2,9 @@ let navlinks = document.querySelector(".nav-links");
         function onToggleMenu() {
             let menu = document.querySelector(".menu");
             if(menu.src.endsWith("menu.png")){
-                menu.src = "/src/img/close.png";
+                menu.src = "../../src/img/close.png";
             }else{
-                menu.src = "/src/img/menu.png";
+                menu.src = "../../src/img/menu.png";
             }
             navlinks.classList.toggle('top-[60px]');
         }
